6c12afb52f3389646ed0e36ca8babc4f51290a83
younes daoudi <dyounes896@gmail.com>
2024-08-02 22:34:09 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,230 @@
+import img1 from "./img/img1.jpg";
+import img2 from "./img/img2.jpg";
+import img3 from "./img/img3.jpg";
+import img4 from "./img/img4.jpg";
+import img5 from "./img/img5.jpg";
+import img6 from "./img/img6.jpg";
+import img7 from "./img/img7.jpg";
+import img8 from "./img/img8.jpg";
+import img9 from "./img/img9.jpg";
+import img10 from "./img/img10.jpg";
+import img11 from "./img/img11.jpg";
+import img12 from "./img/img12.jpg";
+import img13 from "./img/img13.jpg";
+import img14 from "./img/img14.jpg";
+import img15 from "./img/img15.jpg";
+import img16 from "./img/img16.jpg";
+import img17 from "./img/img17.jpg";
+import img18 from "./img/img18.jpg";
+import img19 from "./img/img19.jpg";
+import img20 from "./img/img20.jpg";
+import img21 from "./img/img21.jpg";
+import img22 from "./img/img22.jpg";
+import img23 from "./img/img23.jpg";
+import img24 from "./img/img24.jpg";
+import img25 from "./img/img25.jpg";
+import img26 from "./img/img26.jpg";
+import img27 from "./img/img27.jpg";
+import img28 from "./img/img28.jpg";
+
+const data = {
+  productData: [
+    {
+      id: 1,
+      img: img1,
+      title: "Parure de bureaux",
+      desc: "disponible en deux couleurs",
+      price: 6800,
+    },
+    {
+      id: 2,
+      img: img2,
+      title: "Valise diplomatique ",
+      desc: "veritable cuir ",
+      price: 25000,
+    },
+    {
+      id: 3,
+      img: img3,
+      title: "Parure de bureaux en cuir ",
+      desc: "veritable cuir",
+      price: 68000,
+    },
+    {
+      id: 4,
+      img: img4,
+      title: "Parure de bureaux en cuir ",
+      desc: "veritable cuir",
+      price: 68000,
+    },
+    {
+      id: 5,
+      img: img5,
+      title: "Parure de bureaux en cuir",
+      desc: "veritable cuir",
+      price: 68000,
+    },
+    {
+      id: 6,
+      img: img6,
+      title: "Parure de bureaux en cuir ",
+      desc: "veritable cuir",
+      price: 68000,
+    },
+    {
+      id: 7,
+      img: img7,
+      title: "Note book ",
+      desc: "diponible differente couleur",
+      price: 550,
+    },
+    {
+      id: 8,
+      img: img8,
+      title: "Valise pilote + porte pc ",
+      desc: "s/cuir",
+      price: 35000,
+    },
+    {
+      id: 9,
+      img: img9,
+      title: "Note book",
+      desc: "disponible differente couleurs",
+      price: 550,
+    },
+    {
+      id: 10,
+      img: img10,
+      title: "Valise pilote ",
+      desc: "s/cuir",
+      price: 25000,
+    },
+    {
+      id: 11,
+      img: img11,
+      title: "Valise diplomatique golden ",
+      desc: "veritable cuir",
+      price: 25000,
+    },
+    {
+      id: 12,
+      img: img12,
+      title: "Valise diplomatique golden ",
+      desc: "veritable cuir",
+      price: 25000,
+    },
+    {
+      id: 13,
+      img: img13,
+      title: "Valise diplomatique golden ",
+      desc: "veritable cuir",
+      price: 25000,
+    },
+    {
+      id: 14,
+      img: img14,
+      title: "Valise diplomatique golden ",
+      desc: "veritable cuir",
+      price: 25000,
+    },
+    {
+      id: 15,
+      img: img15,
+      title: "Porte feuille wylson",
+      desc: "veritable cuir",
+      price: 6800,
+    },
+    {
+      id: 16,
+      img: img16,
+      title: "Stylo pierre cardin",
+      desc: "",
+      price: 5800,
+    },
+    {
+      id: 17,
+      img: img17,
+      title: "Parure de stylo dior",
+      desc: "",
+      price: 22000,
+    },
+    {
+      id: 18,
+      img: img18,
+      title: "parure de stylo st dupont",
+      desc: "clip diamnod serpent",
+      price: 22000,
+    },
+    {
+      id: 19,
+      img: img19,
+      title: "Porte feuille wylson",
+      desc: "veritable cuir",
+      price: 6800,
+    },
+    {
+      id: 20,
+      img: img20,
+      title: "Note book ",
+      desc: "disponible differente couleurs",
+      price: 550,
+    },
+    {
+      id: 21,
+      img: img21,
+      title: "Compagnant Fuschia",
+      desc: "veritable cuir",
+      price: 7800,
+    },
+    {
+      id: 22,
+      img: img22,
+      title: "Parure Mont blanc",
+      desc: "Montre + stylo",
+      price: 35000,
+    },
+    {
+      id: 23,
+      img: img23,
+      title: "Parure stylo parker",
+      desc: "deux stylo rollerbill",
+      price: 12500,
+    },
+    {
+      id: 24,
+      img: img24,
+      title: "Porte feuille will polo ",
+      desc: "veritable cuir",
+      price: 5400,
+    },
+    {
+      id: 25,
+      img: img25,
+      title: "Cartable katana ",
+      desc: "veritable cuir",
+      price: 35000,
+    },
+    {
+      id: 26,
+      img: img26,
+      title: "Cartable katana ",
+      desc: "veritable cuir",
+      price: 35000,
+    },
+    {
+      id: 27,
+      img: img27,
+      title: "Cartable katana ",
+      desc: "veritable cuir",
+      price: 35000,
+    },
+    {
+      id: 28,
+      img: img28,
+      title: "Cartable katana ",
+      desc: "veritable cuir",
+      price: 35000,
+    },
+  ],
+};
+export default data;
